8dad0ed79ce4fe5c7ea1e843ebf7eb8efd3265ce
franr99 <frosas@alu.ucam.edu>
2020-02-25 12:39:39 +0100

    primeros pasos

---
+++ Pokemon/Assets/General.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="General" tilewidth="16" tileheight="16" tilecount="2916" columns="54">
+ <image source="../pokemon/General.png" width="864" height="864"/>
+</tileset>
