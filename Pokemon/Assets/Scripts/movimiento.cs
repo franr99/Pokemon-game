10eb4c88f7e053613d2dcd7f9aaae608fb008903
JJbestOf99 <56269172+JJbestOf99@users.noreply.github.com>
2020-02-26 20:33:16 +0100

    movIdle

--- Pokemon/Assets/Scripts/movimiento.cs
+++ Pokemon/Assets/Scripts/movimiento.cs
@@ -5,66 +5,33 @@ using UnityEngine;
 public class movimiento : MonoBehaviour
 {
     public float velocidad;
-    public Vector2 direccion;
+   
     private Animator anime;
-    private SpriteRenderer sprite;
-    private Rigidbody2D rb2d;
+    
     
     // Start is called before the first frame update
     void Start()
     {
         anime = GetComponent<Animator>();
-        sprite = GetComponent<SpriteRenderer>();
-        rb2d = GetComponent<Rigidbody2D>(); // hay que meterle un rigidbody y poner gravity scale a 0 
+        
     }
 
-    // Update is called once per frame
-    /*
-    void FixedUpdate()
-    {
-        float moveHorizontal = Input.GetAxis ("Horizontal");
-        float moveVertical = Input.GetAxis ("Vertical");
-        
-        Vector2 movimiento = new Vector2 (moveHorizontal,moveVertical);
-        rb2d.AddForce(movimiento * velocidad);
-    }
-    */
-    
-    
+  
+
     
     void Update()
     {
-        GetInput();
-        Movimiento();
+        Vector3 mov = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"),0);
+
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + mov, velocidad * Time.deltaTime);
+
+        anime.SetFloat("MovX", mov.x);
+        anime.SetFloat("MovY", mov.y);
+
     }
     
     
-    public void Movimiento()
-    {
-        transform.Translate(direccion * velocidad *Time.deltaTime);
-    }
     
-    public void GetInput()
-    {
-        direccion = Vector2.zero;
-        
-         if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
-         {
-            direccion += Vector2.up;
-         }
-         if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
-         {
-            direccion += Vector2.left;
-         }
-         if(Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
-         {
-            direccion += Vector2.down;
-         }
-         if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-         {
-            direccion += Vector2.right;
-         }
-    }
     
     
     
